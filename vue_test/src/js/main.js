8eaa60786ce081ce05554883bbbebbeed43e3b11
naoyuki_saito <naoyuki_saito@waku-2.com>
2016-11-22 19:44:40 +0900

    add vue.js

---
+++ vue_test/src/js/main.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import { domain, fromNow } from './filters'
+import App from '../vue/App.vue'
+import Page1 from '../vue/pages/page1.vue'
+import Page2 from '../vue/pages/page2.vue'
+// import ItemView from './components/ItemView.vue'
+// import UserView from './components/UserView.vue'
+
+// install router
+Vue.use(Router)
+
+// register filters globally
+// Vue.filter('fromNow', fromNow)
+// Vue.filter('domain', domain)
+
+// routing
+var router = new Router()
+//
+router.map({
+  '/page1': {
+    component: Page1
+},
+  '/page2': {
+    component: Page2
+  }
+  // '/item/:id': {
+  //   component: ItemView
+  // }
+})
+//
+// router.beforeEach(function () {
+//   window.scrollTo(0, 0)
+// })
+
+// router.redirect({
+//   '*': '/news/1'
+// })
+
+router.start(App, '#app')
+
+let test =>() {
+    console.log('ok');
+};
---
+++ vue_test/src/vue/pages/page2.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="news-view" :class="{ loading: !items.length }">
+    page2
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Page2',
+
+  data () {
+    return {
+      page: 1,
+      items: []
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.news-view
+  padding-left 5px
+  padding-right 15px
+  &.loading:before
+    content "Loading..."
+    position absolute
+    top 16px
+    left 20px
+  .nav
+    padding 10px 10px 10px 40px
+    margin-top 10px
+    border-top 2px solid #f60
+    a
+      margin-right 10px
+      &:hover
+        text-decoration underline
+</style>
