a77a7a76963a312e817053b78b92c9ef30b88afc
naoyuki_saito <naoyuki_saito@waku-2.com>
2016-12-21 19:03:20 +0900

    add gipohy

---
+++ giphy/gipfy-vue/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <vue-header></vue-header>
+  </div>
+
+</template>
+
+<script>
+  import VueHeader from './components/Header'
+
+  export default {
+    components: {
+      VueHeader
+    }
+  }
+</script>
